cf12ac8e9bba1468a2375cdadf10d9516b627d59
Miguel Abreu <sapinho79@sapo.pt>
2014-10-19 03:11:00 +0100

    Using since_id to limit the search results

--- refresh.php
+++ refresh.php
@@ -9,8 +9,9 @@ define("OAUTH_TOKEN", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 define("OAUTH_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 // Last sent tweet is stored here
 $last_tweetfn = "lasttweet.txt";
+$last_id = file_get_contents($last_tweetfn);
 // Pattern to search (#feup)
-$feedURL = 'search/tweets.json?q=%23feup&result_type=recent';
+$feedURL = "search/tweets.json?q=%23feup&since_id=$last_id&result_type=recent";
 
 
 function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
@@ -20,37 +21,29 @@ function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
 
 // 
 echo "Starting...<br>";
+echo "Last retweet ID was: $last_id<br>";
 $connection = getConnectionWithAccessToken(OAUTH_TOKEN, OAUTH_SECRET);
 $content = $connection->get($feedURL);
-$tweets = $content[statuses];
+$tweets = $content['statuses'];
 $pos = count($tweets) - 1;
-
-$last_id = file_get_contents($last_tweetfn);
-echo "Last retweet ID was: $last_id<br>";
-
-for($i = $pos; $i >= 0; $i--) { // Start with oldest
-    if(strcmp($last_id, $tweets[$i][id]) == 0) {
-        $pos = $i-1;
-        echo "There are $i new tweets.<br>";
-        break;
-    }
-}
 
 // Retweet new tweets
 if($pos >= 0) {
     echo "New tweets are:<br>";
     for($i = $pos; $i >= 0; $i--) {
          // Retweet
-         $tweet_id = $tweets[$i][id];
-         echo "     - ".$tweets[$i][text]." (id = $tweet_id)<br>";
-         $connection->post("statuses/retweet/$tweet_id");
+         $tweet_id = $tweets[$i]['id_str'];
+         echo "     - ".$tweets[$i]['text']." (id = $tweet_id)<br>";
+         echo $connection->post("statuses/retweet/$tweet_id");
     }
+
+    // Refresh new last tweet
+    $last_id = $tweets[0]['id_str'];
 }
 
-// Refresh new last tweet
-$last = $tweets[0][id];
-echo "New last tweet ID is: $last<br>";
-file_put_contents($last_tweetfn, $last);
+
+echo "New last tweet ID is: $last_id<br>";
+file_put_contents($last_tweetfn, $last_id);
 
 echo "All done!<br>";
 
